30c51e0716548e2954cf8895864068b8db6e405e
GONZAJS <jose.gonzalez@hlag.com>
2022-10-26 13:30:13 +0200

    version 1.0

---
+++ velocitystartgenerator/src/VelocityStartGenerator.java
@@ -0,0 +1,34 @@
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class VelocityStartGenerator {
+
+    static String inputTemplate = "java_example.vm";
+    static String className = "VelocityExample";
+    static String message = "Hello World!";
+    static String outputFile = className + ".java";
+
+    public static void main(String[] args) throws IOException {
+        VelocityEngine velocityEngine = new VelocityEngine();
+        velocityEngine.getTemplate(inputTemplate);
+
+        VelocityContext context = new VelocityContext();
+        context.put("className", className);
+        context.put("message", message);
+
+        Writer writer = new FileWriter(new File(outputFile));
+        String variable = "xxx.vm";
+        velocityEngine.init();
+        Velocity.mergeTemplate(variable, "UTF-8", context, writer);
+        writer.flush();
+        writer.close();
+
+        System.out.println("Generated " + outputFile);
+    }
+}
